14e797e35adbd900fe5d22173f53f26c07b98931
Lukellmann <lukellmann@gmail.com>
2022-12-04 22:24:31 +0100

    Add solution for 2022 Day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
-private val days = setOf<AoCDay>()
+private val days = setOf(
+    aoc2022.Day1,
+)
 
 fun main() = days.forEach(AoCDay::printAnswers)
---
+++ src/main/kotlin/aoc2022/Day1.kt
@@ -0,0 +1,20 @@
+package aoc2022
+
+import AoCDay
+
+// https://adventofcode.com/2022/day/1
+object Day1 : AoCDay(
+    title = "Calorie Counting",
+    part1ExampleAnswer = 24000,
+    part1Answer = 70698,
+    part2ExampleAnswer = 45000,
+    part2Answer = 206643,
+) {
+    private fun caloriesCarriedByElves(input: String) = input
+        .splitToSequence("\n\n")
+        .map { elfInventory -> elfInventory.lineSequence().sumOf(String::toInt) }
+
+    override fun part1(input: String) = caloriesCarriedByElves(input).max()
+
+    override fun part2(input: String) = caloriesCarriedByElves(input).sortedDescending().take(3).sum()
+}
