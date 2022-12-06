397db4a9c507f50f11db594864773d4c8dc5fd88
Lukellmann <lukellmann@gmail.com>
2022-12-06 02:23:33 +0100

    Add solution for 2015 Day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 private val days = listOf(
     aoc2015.Day1,
     aoc2015.Day2,
+    aoc2015.Day3,
 
     aoc2022.Day1,
     aoc2022.Day2,
---
+++ src/main/kotlin/aoc2015/Day3.kt
@@ -0,0 +1,57 @@
+package aoc2015
+
+import AoCDay
+import util.illegalChar
+
+// https://adventofcode.com/2015/day/3
+object Day3 : AoCDay<Int>(
+    title = "Perfectly Spherical Houses in a Vacuum",
+    part1ExampleAnswer = 4,
+    part1Answer = 2592,
+    part2ExampleAnswer = 3,
+    part2Answer = 2360,
+) {
+    private data class House(val x: Int, val y: Int)
+
+    private val START = House(x = 0, y = 0)
+
+    private fun nextHouse(current: House, direction: Char): House {
+        val (x, y) = current
+        return when (direction) {
+            '^' -> House(x, y = y + 1)
+            'v' -> House(x, y = y - 1)
+            '>' -> House(x = x + 1, y)
+            '<' -> House(x = x - 1, y)
+            else -> illegalChar(direction)
+        }
+    }
+
+    override fun part1(input: String): Int {
+        var santa = START
+        val visited = hashSetOf(START)
+
+        for (direction in input) {
+            santa = nextHouse(santa, direction)
+            visited.add(santa)
+        }
+
+        return visited.size
+    }
+
+    override fun part2(input: String): Int {
+        var santa = START
+        var robot = START
+        val visited = hashSetOf(START)
+
+        var toggle = true
+        for (direction in input) {
+            val current = if (toggle) santa else robot
+            val update = nextHouse(current, direction)
+            visited.add(update)
+            if (toggle) santa = update else robot = update
+            toggle = !toggle
+        }
+
+        return visited.size
+    }
+}
